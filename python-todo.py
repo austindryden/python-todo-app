01348320595f9183480522a627a16b55e6fb710f
Austin Dryden <austinisnorobot@gmail.com>
2019-11-13 16:00:36 -0500

    adds all basic functions with minimal error handling

--- python-todo.py
+++ python-todo.py
@@ -5,18 +5,15 @@ def add_todo(todo):
     todo_list.append(todo)
 
 def del_todo(todo):
-    del todo_list[index]
+    del todo_list[todo]
 
 def print_todo():
-    for x in range(len(todo_list)):
-        print(f"{x}. {todo_list[x]}")
+    if todo_list == []:
+        print("you did it all. find more to do.")
+    else:
+        for x in range(len(todo_list)):
+            print(f"{x}. {todo_list[x]}")
 
-# prints empty list
-print_todo()
-#add todo by calling func
-add_todo("feed the cat")
-#print again
-print_todo()
 # i need to be able to delete todods.
 def delete_todo(index):
     try:
@@ -24,3 +21,32 @@ def delete_todo(index):
     except IndexError:
         print("Sorry, no to do at that index")
 
+def main():
+    menu ="""
+A simple little python to do list app
++++++++++++++++++++++++++++++++++++++
+0. Quit
+1. Print the To do list
+2. Add a to do list
+3. Complete a To Do
+"""
+    is_running = True
+    while is_running:
+        print(menu)
+        selection = input("What would you like to do? ")
+        if selection == '0':
+            is_running = False
+            print("BYEEEEEE!!!!!!")
+        elif selection == '1':
+            print_todo()
+        elif selection == '2':
+            add_todo(input("what do you need to do?"))
+
+        elif selection == '3':
+            print_todo()
+            del_todo(int(input("gimme the number of the thing you did : ")))
+        else:
+            print("pick a real thing this time")
+        
+
+main()
